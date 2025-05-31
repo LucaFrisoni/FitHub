e0566890a3eec3825a3325e369c4b7e56833858b
Ramiro <ramiroiperez1804@gmail.com>
2025-05-31 15:15:09 -0300

    prueba_env_reserva

--- Front/app.py
+++ Front/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
    return render_template('home.html')
 
+@app.route('/reservas')
+def reserva():
+   return render_template('reservas.html')
+
 if __name__ == '__main__':
    app.run("localhost", port=3000)
