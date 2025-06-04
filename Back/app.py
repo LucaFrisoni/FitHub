137c753edc688297d14022ba31bcfbd375a44f24
Agustin <agustindecarloo@gmail.com>
2025-06-04 12:14:21 -0300

    Feat: Endpoints AlquieresPlan

--- Back/app.py
+++ Back/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from routes.planes import planes_bp
 from routes.productos import productos_bp
-from routes.reservas import reservas_bp
+from routes.alquileresplan import alquileres_plan_bp
 from routes.usuarios import usuarios_bp
 from routes.roles import roles_bp
 from routes.docs import init_docs
@@ -12,7 +12,7 @@ app = Flask(__name__)
 init_docs(app)
 app.register_blueprint(planes_bp, url_prefix="/planes")
 app.register_blueprint(productos_bp, url_prefix="/productos")
-app.register_blueprint(reservas_bp, url_prefix="/reservas")
+app.register_blueprint(alquileres_plan_bp, url_prefix="/alquileres_plan")
 app.register_blueprint(usuarios_bp, url_prefix="/usuarios")
 app.register_blueprint(roles_bp, url_prefix="/roles")
 
